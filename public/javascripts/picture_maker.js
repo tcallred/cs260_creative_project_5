9e94e13151b82871ca13287b69602a3e2e69c70a
tcallred <taylor.allred21@gmail.com>
2017-11-16 03:57:55 +0000

    Finished front end. Using dummy data for canvases

--- public/javascripts/picture_maker.js
+++ public/javascripts/picture_maker.js
@@ -1,11 +1,41 @@
 angular.module('app', [])
-    .controller('mainCtrl', mainCtrl);
+    .controller('mainCtrl', ['$scope', '$http', mainCtrl]);
 
-function mainCtrl($scope) {
+function mainCtrl($scope, $http) {
+
     $scope.images = [];
+    $scope.canvas = {
+        name: "Untitled",
+        images: []
+    };
+    $scope.canvasArray = [{
+            name: "Test 1",
+            images: [{
+                picName: 'Google',
+                picUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+                picWidth: 500,
+                picIndex: 1,
+                picLeft: 700,
+                picTop: 200,
+                picSelected: false
+            }]
+        },
+        {
+            name: "Test 2",
+            images: [{
+                picName: 'Google',
+                picUrl: 'https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
+                picWidth: 500,
+                picIndex: 1,
+                picLeft: 700,
+                picTop: 200,
+                picSelected: false
+            }]
+        }
+    ]
 
     $scope.addImage = function(img) {
-        $scope.images.push({
+        $scope.canvas.images.push({
             picName: img.name,
             picUrl: img.url,
             picWidth: 500,
@@ -30,5 +60,22 @@ function mainCtrl($scope) {
         } else {
             return '';
         }
+    }
+    $scope.saveCanvas = function(c) {
+
+        console.log(c);
+        $scope.canvasArray.push(c);
+        // return $http.post('/canvases', c).success(function(data) {
+        // $scope.loadAll();
+        // });
+
+    }
+    $scope.loadCanvas = function(c) {
+        $scope.canvas = c;
+    }
+    $scope.loadAll = function() {
+        return $http.get('/canvases').success(function(data) {
+            angular.copy(data, $scope.canvasArray);
+        });
     }
 }
