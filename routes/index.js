034a53273a8cfe76cc8a2811fc895fa2b4a2481c
Alan Hamson <ahamson@gmail.com>
2017-11-17 00:43:05 +0000

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,51 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var mongoose = require('mongoose'); 
+
+mongoose.connect('mongodb://localhost/canvasDB', { useMongoClient: true });
+
+var canvasSchema = mongoose.Schema({
+Name: String,
+Image: [] 
+
+});
+
+var Canvas = mongoose.model('Canvas', canvasSchema);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+console.log('Connected');
+});
+
+
+
+router.post('/canvases', function(req, res, next) {
+console.log("post route");
+   var newcanvas = new Canvas(req.body);
+   console.log(newcanvas);
+   newcanas.save(function(err, post) {
+     if (err) return console.error(err);
+     console.log(post);
+     res.sendStatus(200); 
+  
+  });
+
+});
+
+router.get('/canvases', function(req, res, next) {
+
+    Canvas.find(function(err, canvasList) {
+     if (err) return console.error(err);
+     else {
+       console.log(canvasList);
+       res.json(canvasList);
+    }
+  })
+
+
+});
 
 
 module.exports = router;
